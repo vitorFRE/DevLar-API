60eecc1ea69cd0de6abb5a47d582dbb2c7d4ae56
Vitor <frete497@gmail.com>
2023-04-08 20:01:07 -0400

    chore: add eslint, standard ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
-
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT,
-}).then(() => {
-  console.log('👌 Server is running on port 3333 👍');
-})
+app.listen(
+  {
+    host: '0.0.0.0',
+    port: env.PORT
+  },
+  (): void => {
+    console.log('👌 Server is running on port 3333 👍')
+  }
+)
