36d64182b28e40e3daa90800ab506256531797ae
Vitor <frete497@gmail.com>
2023-04-11 20:55:34 -0400

    test(registerUseCase): should not create a new user with an existing email

--- src/use-cases/registerUseCase.spec.ts
+++ src/use-cases/registerUseCase.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryUsersRepository } from '@/repositories/inMemory/InMemoryUsersRepository'
 import { RegisterUseCase } from './registerUseCase'
 import { beforeEach, describe, expect, it } from 'vitest'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 let usersRepository: InMemoryUsersRepository
 let sut: RegisterUseCase
@@ -19,5 +20,22 @@ describe('RegisterUseCase test', () => {
     })
     console.log(user)
     expect(user).toHaveProperty('id')
+  })
+
+  it('should not create a new user with an existing email', async () => {
+    await sut.execute({
+      name: 'John Doe',
+      email: 'jaexiste@email.com',
+      password: '123456'
+    })
+
+    await expect(
+      async () =>
+        await sut.execute({
+          name: 'John Doe',
+          email: 'jaexiste@email.com',
+          password: '123456'
+        })
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
