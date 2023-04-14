d6a05b3c0fdb8ef6307b6f74c83230b0f3008a70
Vitor <frete497@gmail.com>
2023-04-14 17:21:26 -0400

    refactor: rename BRcryptHashGenerator class to BcryptHashGenerator

--- src/utils/BCryptHashGenerator.ts
+++ src/utils/BCryptHashGenerator.ts
@@ -1,7 +1,7 @@
 import { hash } from 'bcryptjs'
 import type { PasswordHashGenerator } from '@/@types/PasswordHashGeneratorType'
 
-export class BRcryptHashGenerator implements PasswordHashGenerator {
+export class BcryptHashGenerator implements PasswordHashGenerator {
   async generate(value: string): Promise<string> {
     const saltRounds = 6
     const hashedValue = await hash(value, saltRounds)
