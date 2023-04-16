b8cf3ab1e363fb5556c19e6d000824205f81c835
Vitor <frete497@gmail.com>
2023-04-16 13:59:55 -0400

    test(register): should not create a new user with an invalid password

--- src/use-cases/registerUseCase.spec.ts
+++ src/use-cases/registerUseCase.spec.ts
@@ -5,11 +5,14 @@ import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 import { compare } from 'bcryptjs'
 import { EmailValidatorAdapter } from '@/utils/EmailValidator'
 import { InvalidEmailError } from './errors/InvalidEmailError'
+import { InvalidPasswordError } from './errors/InvalidPasswordError'
 import { BcryptHashGenerator } from '@/utils/BcryptHashGenerator'
+import { PasswordValidatorAdapter } from '@/utils/PasswordValidator'
 
 let usersRepository: InMemoryUsersRepository
 let emailValidator: EmailValidatorAdapter
 let passwordHashGenerator: BcryptHashGenerator
+let passowrdValidator: PasswordValidatorAdapter
 let sut: RegisterUseCase
 
 describe('RegisterUseCase test', () => {
@@ -17,14 +20,20 @@ describe('RegisterUseCase test', () => {
     usersRepository = new InMemoryUsersRepository()
     emailValidator = new EmailValidatorAdapter()
     passwordHashGenerator = new BcryptHashGenerator()
-    sut = new RegisterUseCase(usersRepository, emailValidator, passwordHashGenerator)
+    passowrdValidator = new PasswordValidatorAdapter()
+    sut = new RegisterUseCase(
+      usersRepository,
+      emailValidator,
+      passwordHashGenerator,
+      passowrdValidator
+    )
   })
 
   it('should create a new user', async () => {
     const { user } = await sut.execute({
       name: 'John Doe',
       email: 'email@email.com',
-      password: '123456'
+      password: 'minhasenha777'
     })
     expect(user).toHaveProperty('id')
   })
@@ -33,7 +42,7 @@ describe('RegisterUseCase test', () => {
     await sut.execute({
       name: 'John Doe',
       email: 'jaexiste@email.com',
-      password: '123456'
+      password: 'minhasenha777'
     })
 
     await expect(
@@ -41,7 +50,7 @@ describe('RegisterUseCase test', () => {
         await sut.execute({
           name: 'John Doe',
           email: 'jaexiste@email.com',
-          password: '123456'
+          password: 'minhasenha777'
         })
     ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
@@ -49,11 +58,11 @@ describe('RegisterUseCase test', () => {
   it('password should be hashed', async () => {
     const { user } = await sut.execute({
       name: 'John Doe',
-      email: 'jaexiste@email.com',
-      password: '123456'
+      email: 'meuemail@email.com',
+      password: 'minhasenha777'
     })
 
-    const isPasswordHashed = await compare('123456', user.password_hash)
+    const isPasswordHashed = await compare('minhasenha777', user.password_hash)
 
     expect(isPasswordHashed).toBe(true)
   })
@@ -64,8 +73,19 @@ describe('RegisterUseCase test', () => {
         await sut.execute({
           name: 'John Doe',
           email: 'invalid_email',
-          password: '123456'
+          password: 'minhasenha777'
         })
     ).rejects.toBeInstanceOf(InvalidEmailError)
+  })
+
+  it('should not create a new user whi an invalid password', async () => {
+    await expect(
+      async () =>
+        await sut.execute({
+          name: 'John Doe',
+          email: 'meuemail@email.com',
+          password: 'senhai'
+        })
+    ).rejects.toBeInstanceOf(InvalidPasswordError)
   })
 })
